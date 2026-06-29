27097b0a9d86e3bdbc2ee02bda92c1016be70c2a
mohakkapoor4 <mohak.kapoor.v@gmail.com>
2026-06-29 10:14:54 +0530

    playing around with batch embeddings

--- src/embeddings_deep.py
+++ src/embeddings_deep.py
@@ -15,6 +15,21 @@ def basic_embeddings():
     print(f"First 5 values: {single_embedding[:5]}")
     print(f"Vector norm: {np.linalg.norm(single_embedding):.4f}")
 
+def batch_embeddings():
+    text = [
+        "What is Machine Learning",
+        "What is Deep Learning",
+        "What is Artificial Intelligence",
+    ]
+    embeddings = embeddings_model.embed_documents(text)
+    for i,emb in enumerate(embeddings):
+        print(f"Text {i+1} : {text[i]}")
+        print(f"Vector Dimensions: {len(emb)}")
+        print(f"First 5 values: {emb[:5]}")
+        print(f"Vector norm: {np.linalg.norm(emb):.4f}")
+        
+
 
 if __name__ == "__main__":
-    basic_embeddings()
+    # basic_embeddings()
+    batch_embeddings()
